32b6ce9126264d055440920be74ea5b36ba98941
andryukova <olyaandryukova@gmail.com>
2026-09-09 13:56:22 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -95,7 +95,7 @@ async function loadCats() {
     }
 
     try {
-        const response = await fetch(`data.json?v=${Date.now()}`);
+        const response = await fetch('data.json?v=1.0.1');
 
         if (!response.ok) {
             throw new Error(
